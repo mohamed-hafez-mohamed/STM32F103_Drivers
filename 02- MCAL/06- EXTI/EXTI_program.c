e7ae1183c8b1968a0fb9bebecc952c63903e4e74
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2020-08-31 13:07:39 +0200

    Add files via upload

--- 02- MCAL/06- EXTI/EXTI_program.c
+++ 02- MCAL/06- EXTI/EXTI_program.c
@@ -43,39 +43,9 @@
 /******************************************************************************
 * Module Variable Definitions
 *******************************************************************************/
-static EXTI_voidPtrToFunction EXTI0_PtrFunction;
-static EXTI_voidPtrToFunction EXTI1_PtrFunction;
-static EXTI_voidPtrToFunction EXTI2_PtrFunction;
-static EXTI_voidPtrToFunction EXTI3_PtrFunction;
-static EXTI_voidPtrToFunction EXTI4_PtrFunction;
-static EXTI_voidPtrToFunction EXTI5_PtrFunction;
-static EXTI_voidPtrToFunction EXTI6_PtrFunction;
-static EXTI_voidPtrToFunction EXTI7_PtrFunction;
-static EXTI_voidPtrToFunction EXTI8_PtrFunction;
-static EXTI_voidPtrToFunction EXTI9_PtrFunction;
-static EXTI_voidPtrToFunction EXTI10_PtrFunction;
-static EXTI_voidPtrToFunction EXTI11_PtrFunction;
-static EXTI_voidPtrToFunction EXTI12_PtrFunction;
-static EXTI_voidPtrToFunction EXTI13_PtrFunction;
-static EXTI_voidPtrToFunction EXTI14_PtrFunction;
-static EXTI_voidPtrToFunction EXTI15_PtrFunction;
+static EXTI_voidPtrToFunction  EXTI_PtrFunction [EXTI_NUMBER_OF_EXTI];
+static EXTI_voidPtrToParameter EXTI_PtrParameter[EXTI_NUMBER_OF_EXTI];
 
-void * EXTI0_Parameter;
-void * EXTI1_Parameter;
-void * EXTI2_Parameter;
-void * EXTI3_Parameter;
-void * EXTI4_Parameter;
-void * EXTI5_Parameter;
-void * EXTI6_Parameter;
-void * EXTI7_Parameter;
-void * EXTI8_Parameter;
-void * EXTI9_Parameter;
-void * EXTI10_Parameter;
-void * EXTI11_Parameter;
-void * EXTI12_Parameter;
-void * EXTI13_Parameter;
-void * EXTI14_Parameter;
-void * EXTI15_Parameter;
 /******************************************************************************
 * Function Prototypes
 *******************************************************************************/
@@ -178,22 +148,22 @@ u8 EXTI_u8InitISRWithFunction(u8 Copy_Index, EXTI_InterruptId_t Copy_ExtiId[], E
     {
        switch(Copy_ExtiId[Local_u8Counter])
        {
-            case EXTI0_ID:  EXTI0_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI0_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI1_ID:  EXTI1_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI1_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI2_ID:  EXTI2_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI2_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI3_ID:  EXTI3_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI3_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI4_ID:  EXTI4_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI4_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI5_ID:  EXTI5_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI5_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI6_ID:  EXTI6_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI6_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI7_ID:  EXTI7_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI7_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI8_ID:  EXTI8_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI8_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI9_ID:  EXTI9_PtrFunction  = Copy_PtrFunction[Local_u8Counter]; EXTI9_Parameter  = Copy_Parameter[Local_u8Counter];break;
-            case EXTI10_ID: EXTI10_PtrFunction = Copy_PtrFunction[Local_u8Counter]; EXTI10_Parameter = Copy_Parameter[Local_u8Counter];break;
-            case EXTI11_ID: EXTI11_PtrFunction = Copy_PtrFunction[Local_u8Counter]; EXTI11_Parameter = Copy_Parameter[Local_u8Counter];break;
-            case EXTI12_ID: EXTI12_PtrFunction = Copy_PtrFunction[Local_u8Counter]; EXTI12_Parameter = Copy_Parameter[Local_u8Counter];break;
-            case EXTI13_ID: EXTI13_PtrFunction = Copy_PtrFunction[Local_u8Counter]; EXTI13_Parameter = Copy_Parameter[Local_u8Counter];break;
-            case EXTI14_ID: EXTI14_PtrFunction = Copy_PtrFunction[Local_u8Counter]; EXTI14_Parameter = Copy_Parameter[Local_u8Counter];break;
-            case EXTI15_ID: EXTI15_PtrFunction = Copy_PtrFunction[Local_u8Counter]; EXTI15_Parameter = Copy_Parameter[Local_u8Counter];break;
+            case EXTI0_ID :EXTI_PtrFunction [EXTI0_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI0_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI1_ID :EXTI_PtrFunction [EXTI1_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI1_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI2_ID :EXTI_PtrFunction [EXTI2_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI2_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI3_ID :EXTI_PtrFunction [EXTI3_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI3_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI4_ID :EXTI_PtrFunction [EXTI4_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI4_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI5_ID :EXTI_PtrFunction [EXTI5_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI5_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI6_ID :EXTI_PtrFunction [EXTI6_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI6_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI7_ID :EXTI_PtrFunction [EXTI7_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI7_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI8_ID :EXTI_PtrFunction [EXTI8_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI8_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI9_ID :EXTI_PtrFunction [EXTI9_ID]=Copy_PtrFunction[Local_u8Counter]; EXTI_PtrParameter[EXTI9_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI10_ID:EXTI_PtrFunction[EXTI10_ID]=Copy_PtrFunction[Local_u8Counter];EXTI_PtrParameter[EXTI10_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI11_ID:EXTI_PtrFunction[EXTI11_ID]=Copy_PtrFunction[Local_u8Counter];EXTI_PtrParameter[EXTI11_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI12_ID:EXTI_PtrFunction[EXTI12_ID]=Copy_PtrFunction[Local_u8Counter];EXTI_PtrParameter[EXTI12_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI13_ID:EXTI_PtrFunction[EXTI13_ID]=Copy_PtrFunction[Local_u8Counter];EXTI_PtrParameter[EXTI13_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI14_ID:EXTI_PtrFunction[EXTI14_ID]=Copy_PtrFunction[Local_u8Counter];EXTI_PtrParameter[EXTI14_ID]=Copy_Parameter[Local_u8Counter];break;
+            case EXTI15_ID:EXTI_PtrFunction[EXTI15_ID]=Copy_PtrFunction[Local_u8Counter];EXTI_PtrParameter[EXTI15_ID]=Copy_Parameter[Local_u8Counter];break;
        }
     }
 }
@@ -201,68 +171,68 @@ u8 EXTI_u8InitISRWithFunction(u8 Copy_Index, EXTI_InterruptId_t Copy_ExtiId[], E
 
 void EXTI0_IRQHandler(void)
 {
-    EXTI0_PtrFunction(EXTI0_Parameter);
+    EXTI_PtrFunction[EXTI0_ID](EXTI_PtrParameter[EXTI0_ID]);
 }
 
 void EXTI1_IRQHandler(void)
 {
-    EXTI1_PtrFunction(EXTI1_Parameter);
+    EXTI_PtrFunction[EXTI1_ID](EXTI_PtrParameter[EXTI1_ID]);
 }
 void EXTI2_IRQHandler(void)
 {
-    EXTI2_PtrFunction(EXTI2_Parameter);
+    EXTI_PtrFunction[EXTI2_ID](EXTI_PtrParameter[EXTI2_ID]);
 }
 void EXTI3_IRQHandler(void)
 {
-    EXTI3_PtrFunction(EXTI3_Parameter);
+    EXTI_PtrFunction[EXTI3_ID](EXTI_PtrParameter[EXTI3_ID]);
 }
 void EXTI4_IRQHandler(void)
 {
-    EXTI4_PtrFunction(EXTI4_Parameter);
+    EXTI_PtrFunction[EXTI4_ID](EXTI_PtrParameter[EXTI4_ID]);
 }
 void EXTI5_IRQHandler(void)
 {
-    EXTI5_PtrFunction(EXTI5_Parameter);
+    EXTI_PtrFunction[EXTI5_ID](EXTI_PtrParameter[EXTI5_ID]);
 }
 void EXTI6_IRQHandler(void)
 {
-    EXTI6_PtrFunction(EXTI6_Parameter);
+    EXTI_PtrFunction[EXTI6_ID](EXTI_PtrParameter[EXTI6_ID]);
 }
 void EXTI7_IRQHandler(void)
 {
-    EXTI7_PtrFunction(EXTI7_Parameter);
+    EXTI_PtrFunction[EXTI7_ID](EXTI_PtrParameter[EXTI7_ID]);
 }
 void EXTI8_IRQHandler(void)
 {
-    EXTI8_PtrFunction(EXTI8_Parameter);
+    EXTI_PtrFunction[EXTI8_ID](EXTI_PtrParameter[EXTI8_ID]);
 }
 void EXTI9_IRQHandler(void)
 {
-    EXTI9_PtrFunction(EXTI9_Parameter);
+    EXTI_PtrFunction[EXTI9_ID](EXTI_PtrParameter[EXTI9_ID]);
 }
 void EXTI10_IRQHandler(void)
 {
-    EXTI10_PtrFunction(EXTI10_Parameter);
+    EXTI_PtrFunction[EXTI10_ID](EXTI_PtrParameter[EXTI11_ID]);
 }
 void EXTI11_IRQHandler(void)
 {
-    EXTI11_PtrFunction(EXTI12_Parameter);
+    EXTI_PtrFunction[EXTI11_ID](EXTI_PtrParameter[EXTI11_ID]);
 }
 void EXTI12_IRQHandler(void)
 {
-    EXTI12_PtrFunction(EXTI12_Parameter);
+    EXTI_PtrFunction[EXTI12_ID](EXTI_PtrParameter[EXTI12_ID]);
 }
 void EXTI13_IRQHandler(void)
 {
-    EXTI13_PtrFunction(EXTI13_Parameter);
+    EXTI_PtrFunction[EXTI13_ID](EXTI_PtrParameter[EXTI13_ID]);
 }
 void EXTI14_IRQHandler(void)
 {
-    EXTI14_PtrFunction(EXTI14_Parameter);
+    EXTI_PtrFunction[EXTI14_ID](EXTI_PtrParameter[EXTI14_ID]);
 }
 void EXTI15_IRQHandler(void)
 {
-    EXTI15_PtrFunction(EXTI15_Parameter);
+    EXTI_PtrFunction[EXTI15_ID](EXTI_PtrParameter[EXTI15_ID]);
 }
 
 /*************** END OF FUNCTIONS ***************************************************************************/
